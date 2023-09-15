0c85d7fdf150bf8c94c5e9edaa4c42f94581c867
wk <somnuk.wk@gmail.com>
2023-09-15 14:02:54 +0700

    Watermark

--- src/MyViewer/wwwroot/js/webviewerScripts.js
+++ src/MyViewer/wwwroot/js/webviewerScripts.js
@@ -46,6 +46,29 @@ window.webviewerFunctions = {
                     }
                 });
             });
+
+             Core.documentViewer.setWatermark({
+                  // Draw diagonal watermark in middle of the document
+                  diagonal: {
+                    fontSize: 25, // or even smaller size
+                    fontFamily: 'sans-serif',
+                    color: 'red',
+                    opacity: 50, // from 0 to 100
+                    text: 'Watermark'
+                  },
+
+                  // Draw header watermark
+                  header: {
+                    fontSize: 10,
+                    fontFamily: 'sans-serif',
+                    color: 'red',
+                    opacity: 70,
+                    left: 'left watermark',
+                    center: 'center watermark',
+                    right: ''
+                  }
+            });
+
         })
     }
 };
